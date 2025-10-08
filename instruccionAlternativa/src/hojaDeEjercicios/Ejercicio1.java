3a5423cad8279d882597b76fbb8df965877da5cc
chrx <alumno.707084@ies-azarquiel.es>
2025-10-08 10:46:56 +0200

    &/&/

--- instruccionAlternativa/src/hojaDeEjercicios/Ejercicio1.java
+++ instruccionAlternativa/src/hojaDeEjercicios/Ejercicio1.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-//		1. Diseñar un algoritmo para leer las longitudes de los lados de un triángulo (A, B,
-//		C) y determinar qué tipo de triángulo es, de acuerdo a los siguientes casos.
+//	1. Diseñar un algoritmo para leer las longitudes de los lados de un triángulo (A, B,
+//	C) y determinar qué tipo de triángulo es, de acuerdo a los siguientes casos.
 //	Suponiendo que A es el mayor de los lados y que B y C corresponden a los otros
 //	dos lados entonces:
 //
@@ -30,12 +30,16 @@ public class Ejercicio1 {
 		
 		if (A >= B + C) {
 			System.out.println("NO es un triángulo");
+			
 		} else if (Math.pow(A, 2) == Math.pow(B, 2) + Math.pow(C, 2)) {
 			System.out.println("Es un triángulo rectángulo");
+			
 		} else if (Math.pow(A, 2) > Math.pow(B, 2) + Math.pow(C, 2)) {
 			System.out.println("Es un triángulo obtusángulo");
+			
 		} else if (Math.pow(A, 2) < Math.pow(B, 2) + Math.pow(C, 2)) {
 			System.out.println("Es un triánglo acutángulo");
+
 		}
 		
 	}
