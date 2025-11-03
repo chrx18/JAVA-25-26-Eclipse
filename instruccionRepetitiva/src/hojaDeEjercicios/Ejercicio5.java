30a7df47dbb0deb46e57a0930b4f33ed41f23942
chrx <alumno.707084@ies-azarquiel.es>
2025-11-03 10:23:07 +0100

    9876

--- instruccionRepetitiva/src/hojaDeEjercicios/Ejercicio5.java
+++ instruccionRepetitiva/src/hojaDeEjercicios/Ejercicio5.java
@@ -1,11 +1,35 @@
 package hojaDeEjercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner (System.in);
+		int cuantos, nuevo, anterior, antesDeAnterior;
+		
+		System.out.println("Cuantos números imprimo??: ");
+		cuantos = sc.nextInt();
+		
+		System.out.print("0 1 "); //los dos primeros los imprimo tal cual
+		antesDeAnterior = 0;
+		anterior = 1;			 //anoto los valores en las variables anterior
+		
+		for (int i = 3; i <= cuantos; i++) {
+			
+			//calcular el nuevo número
+			nuevo = antesDeAnterior + anterior;
+			
+			//lo muestro en pantalla
+			System.out.println(nuevo + " ");
+			
+			//me preparo para la vuelta siguiente
+			antesDeAnterior = anterior;
+			anterior = nuevo;
+		}
 		
 	}
 
-}
+}	
