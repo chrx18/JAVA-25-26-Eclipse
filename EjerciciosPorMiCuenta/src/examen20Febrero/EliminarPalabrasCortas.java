a38689e9d6c5add0a756efd8d0f1cdfd78d23410
chrx <alumno.707084@ies-azarquiel.es>
2026-02-11 19:19:32 +0100

    11/02/26

--- EjerciciosPorMiCuenta/src/examen20Febrero/EliminarPalabrasCortas.java
+++ EjerciciosPorMiCuenta/src/examen20Febrero/EliminarPalabrasCortas.java
@@ -2,31 +2,37 @@ package examen20Febrero;
 
 public class EliminarPalabrasCortas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        String texto = "El bar de la plaza";
-
+    public static void main(String[] args) {
+        String texto = "El perro de Juan corre rápido";
+        
         StringBuilder palabraActual = new StringBuilder();
         StringBuilder resultado = new StringBuilder();
         
-
         for (int i = 0; i < texto.length(); i++) {
             char c = texto.charAt(i);
-
+            
             if (c != ' ') {
                 palabraActual.append(c);
+                
             } else {
-                if (palabraActual.length() >= 3) {
-                    resultado.append(palabraActual).append(" ");
+            	
+                if (palabraActual.length() >= 4) {
+                    if (resultado.length() > 0) {
+                        resultado.append(" ");
+                    }
+                    resultado.append(palabraActual);
                 }
                 palabraActual.setLength(0);
             }
         }
 
-        if (palabraActual.length() >= 3) {
+        if (palabraActual.length() >= 4) {
+            if (resultado.length() > 0) {
+                resultado.append(" ");
+            }
             resultado.append(palabraActual);
         }
-
+        
         System.out.println(resultado.toString());
     }
 }
